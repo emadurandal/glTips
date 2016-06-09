32425f6af12fc24eb50938ed10892fe4de4f7f87
Yuki Shimada <yukiema@gmail.com>
2016-06-09 12:17:20 +0900

    Add "basic" example.

---
+++ examples/basic/main.js
@@ -0,0 +1,101 @@
+(function (){
+  var canvas = document.querySelector("#webgl-canvas");
+  var gl = glTips.getWebGL1Context(canvas);
+  if (gl) {
+    console.log("WebGL1 context was created successfully.");
+  }
+  gl.clearColor(0.8, 0.8, 1, 1);
+
+
+  var shaderProgram = null;
+  var attribLocationPosition = null;
+  var attribLocationNormal = null;
+
+  function initVertexBuffers()
+  {
+    // create VBO
+    var vbo = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+
+    // convert separeted vertex attribute arrays to a interleaved array
+    var typedArray = glTips.makeVerticesDataInterleaved(gl, gl.FLOAT,
+      [fighterPosArray, fighterNormalArray, fighterTexcoordArray],
+      [3, 3, 2]
+    )
+
+    gl.bufferData(gl.ARRAY_BUFFER, typedArray, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(attribLocationPosition, 3, gl.FLOAT, gl.FALSE, 32, 0)
+    gl.vertexAttribPointer(attribLocationNormal, 3, gl.FLOAT, gl.FALSE, 32, 12)
+    gl.vertexAttribPointer(attribLocationTexcoord, 2, gl.FLOAT, gl.FALSE, 32, 24)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+
+    g_ibo = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, g_ibo);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(fighterIndexArray), gl.STATIC_DRAW);
+  }
+
+  function initShader() {
+    var vertShaderText = document.querySelector("#shader-vs").textContent;
+    var fragShaderText = document.querySelector("#shader-fs").textContent;
+    shaderProgram = glTips.setupShaderProgramFromSource(gl, vertShaderText, fragShaderText);
+
+    attribLocationPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.enableVertexAttribArray(attribLocationPosition);
+    attribLocationNormal = gl.getAttribLocation(shaderProgram, "aNormal");
+    gl.enableVertexAttribArray(attribLocationNormal);
+    attribLocationTexcoord = gl.getAttribLocation(shaderProgram, "aTexcoord");
+    gl.enableVertexAttribArray(attribLocationTexcoord);
+
+
+    gl.useProgram(shaderProgram);
+  }
+
+  var rotation = 0.0;
+  function initMatrix() {
+    var viewMat = mat4.create();
+    mat4.lookAt(viewMat, vec3.fromValues(0, 0, 7), vec3.fromValues(0, 0, 0), vec3.fromValues(0, 1, 0));
+
+    var worldMat = mat4.create();
+    mat4.rotateY(worldMat, worldMat, rotation);
+    rotation += 0.02;
+
+    var projMat = mat4.create();
+    mat4.perspective(projMat, Math.PI / 4, 1, 1, 1000);
+
+    var pvwMat = mat4.create();
+    mat4.mul(pvwMat, viewMat, worldMat);
+    mat4.mul(pvwMat, projMat, pvwMat);
+
+    uniformLocationPVWMatrix = gl.getUniformLocation(shaderProgram, 'uPVWMatrix');
+    gl.uniformMatrix4fv(uniformLocationPVWMatrix, false, pvwMat);
+    uniformLocationWMatrix = gl.getUniformLocation(shaderProgram, 'uWMatrix');
+    gl.uniformMatrix4fv(uniformLocationWMatrix, false, worldMat);
+  }
+
+  function render(){
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    initMatrix();
+
+    gl.drawElements(gl.TRIANGLES, fighterIndexArray.length, gl.UNSIGNED_SHORT, 0);
+
+    requestAnimationFrame( render );
+
+  }
+
+  // glTips();
+
+  initShader();
+  initVertexBuffers();
+  glTips.setupTexture2DSimple(gl, "../assets/fighter.png", gl.RGBA, gl.RGBA,
+    gl.UNSIGNED_BYTE, gl.LINEAR, gl.CLAMP_TO_EDGE, true,
+    function(){
+      var uniformTextureSampler_0 = gl.getUniformLocation(shaderProgram, 'uTexture');
+      gl.uniform1i(uniformTextureSampler_0, 0);
+
+      render();
+    }.bind(this));
+})();
--- glTips.js
+++ glTips.js
@@ -24,8 +24,9 @@
   glTips.compileShader = glTips_compileShader; // glTips#compileShader(gl:WebGLRenderingContext, shaderType:[gl.VERTEX_SHADER|gl.FRAGMENT_SHADER], shaderText:string):WebGLShader
   glTips.setupShaderProgram = glTips_setupShaderProgram; // glTips#setupShaderProgram(gl:WebGLRenderingContext, vertexShader:WebGLShader, fragmentShader:WebGLShader):WebGLShaderProgram
   glTips.setupShaderProgramFromSource = glTips_setupShaderProgramFromSource; // glTips#setupShaderProgramFromSource(gl, vertexShaderText:string, fragmentShaderText:string):WebGLShaderProgram
-
-
+  glTips.setupTexture2D = glTips_setupTexture2D;// glTips#setupTexture2D(gl:WebGLRenderingContext, srcUri:string, level:number, internalFormat:number, format:number, type:number, magFileter:number, minFilter:number, wrapS:number, wrapT:number, mipmap:boolean, flipY:boolean, callback:Function)
+  glTips.setupTexture2DSimple = glTips_setupTexture2DSimple; // glTips#setupTexture2DSimple(gl:WebGLRenderingContext, srcUri, internalFormat, format, type, filter, wrap, flipY, callback)
+  glTips.makeVerticesDataInterleaved = glTips_makeVerticesDataInterleaved; // glTips#makeVerticesDataInterleaved(gl:WebGLRenderingContext, type:number, arrayOfAttributeDataArray:Array, componentArray:Array):TypedArray
 // Implementation ---------------------------------------
 
   /**
@@ -140,6 +141,106 @@
     gl.deleteShader(fragShader);
 
     return shaderProgram;
+  }
+
+  /**
+   * Setup a texture.
+   *
+   * @param {WebGLRenderingContext} gl. WebGL context
+   * @param {string} srcUri. Image source URI.
+   * @param {number} level. Mipmap level.
+   * @param {number} internalFormat. for example, gl.RGBA, gl.RGBA16F.
+   * @param {number} format. format. for example, gl.RGBA.
+   * @param {number} type. data type. for example, gl.UNSIGNED_BYTE.
+   * @param {number} magFileter. for example, gl.LINEAR.
+   * @param {number} minFileter. for example, gl.LINEAR.
+   * @param {number} wrapS. for example, gl.CLAMP_TO_EDGE.
+   * @param {number} wrapT. for example, gl.CLAMP_TO_EDGE.
+   * @param {boolean} mipmap. generate Mipmap or not
+   * @param {boolean} flipY. enable gl.UNPACK_FLIP_Y_WEBGL.
+   * @param {Function} callback. callback called when setup is done.
+   * @memberOf module:glTips
+   */
+  function glTips_setupTexture2D(gl, srcUri, level, internalFormat, format, type,
+    magFileter, minFilter, wrapS, wrapT, mipmap, flipY, callback) {
+    var img = new Image();
+    img.onload = function() {
+      gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flipY);
+      var texture = gl.createTexture();
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, format, type, img);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, magFileter);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, minFilter);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapS);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapT);
+      if (mipmap) {
+        gl.generateMipmap(gl.TEXTURE_2D);
+      }
+      callback(texture);
+    };
+
+    img.src = srcUri;
+  }
+
+  /**
+  * Setup a texture (Simple Version).
+  *
+  * @param {WebGLRenderingContext} gl. WebGL context
+  * @param {string} srcUri. Image source URI.
+  * @param {number} internalFormat. for example, gl.RGBA, gl.RGBA16F.
+  * @param {number} format. format. for example, gl.RGBA.
+  * @param {number} type. data type. for example, gl.UNSIGNED_BYTE.
+  * @param {number} fileter. for example, gl.LINEAR.
+  * @param {number} wrap. for example, gl.CLAMP_TO_EDGE.
+  * @param {boolean} flipY. enable gl.UNPACK_FLIP_Y_WEBGL.
+  * @param {Function} callback. callback called when setup is done.
+  * @memberOf module:glTips
+  */
+  function glTips_setupTexture2DSimple(gl, srcUri, internalFormat, format, type,
+    filter, wrap, flipY, callback) {
+      glTips.setupTexture2D(gl, srcUri, 0, internalFormat, format, type,
+        filter, filter, wrap, wrap, true, flipY, callback);
+  }
+
+  /**
+   * Convert separeted vertex attribute arrays to a interleaved array.
+   *
+   * @param {WebGLRenderingContext} gl. WebGL context
+   * @param {number} type. for example, gl.FLOAT
+   * @param {Array} arrayOfAttributeDataArray. array of vertex attribute data array.
+   * @param {Array} componentArray. array of component number of vertex attribute data array.
+   * @return {TypedArray} a interleaved TypedArray object. for example, Float32Array.
+   * @memberOf module:glTips
+   */
+  function glTips_makeVerticesDataInterleaved(gl, type, arrayOfAttributeDataArray, componentArray) {
+    var bufferSize = 0;
+    for (var key in arrayOfAttributeDataArray) {
+      bufferSize += arrayOfAttributeDataArray[key].length;
+    }
+
+    var componentSize = 0;
+    for (var key in componentArray) {
+      componentSize += componentArray[key];
+    }
+
+    var typedArray = null;
+    if (type === gl.FLOAT) {
+      typedArray = new Float32Array(bufferSize);
+    }
+
+    var offset = 0;
+    var dataLengthAsVector = arrayOfAttributeDataArray[0].length / componentArray[0];
+    for (var i=0; i<dataLengthAsVector; i++) {
+      offset = 0;
+      for (var j=0; j<componentArray.length; j++) {
+        for (var k=0; k<componentArray[j]; k++) {
+          typedArray[i*componentSize + offset + k] = arrayOfAttributeDataArray[j][i*componentArray[j]+k];
+        }
+        offset += componentArray[j];
+      }
+    }
+
+    return typedArray;
   }
 
 // Exports ----------------------------------------------
